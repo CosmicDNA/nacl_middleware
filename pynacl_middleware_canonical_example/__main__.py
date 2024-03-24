6ad07b040328423dc8ff99c4a9d9384d55afe441
Daniel at CosmicDNA <daniel@cosmicdna.co.uk>
2024-03-25 00:06:56 +0400

    :fire: Remove unnecessary files.

--- pynacl_middleware_canonical_example/__main__.py
+++ pynacl_middleware_canonical_example/__main__.py
@@ -2,9 +2,9 @@ from pynacl_middleware_canonical_example.manager import EngineServerManager
 from asyncio import get_event_loop
 
 async def main():
-  esm = EngineServerManager()
-  esm.start()
-  esm.join()
+    esm = EngineServerManager()
+    esm.start()
+    esm.join()
 
 if __name__ == "__main__":
     loop = get_event_loop()
--- pynacl_middleware_canonical_example/websocket/views.py
+++ pynacl_middleware_canonical_example/websocket/views.py
@@ -1,8 +1,6 @@
 """The views / handlers for the server."""
 
 from aiohttp.web import Request, Response
-from pynacl_middleware_canonical_example.logger import log
-from pynacl_middleware_canonical_example.websocket.app_keys import app_keys
 
 async def index(request: Request) -> Response:
     """Index endpoint for the server. Not really needed.
