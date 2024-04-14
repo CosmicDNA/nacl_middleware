cdba7051f091d31d6e57779383d7922916a5919e
Daniel at CosmicDNA <daniel@cosmicdna.co.uk>
2024-04-14 02:26:08 +0100

    :memo: Fix theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,8 +5,6 @@
 import os
 import sys
 
-import sphinx_rtd_theme
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
